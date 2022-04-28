bb5d71ae9d96ff3977462e90c0660ec7f0fd16ec
Luvlee Wreck <luvleewreck@gmail.com>
2022-04-27 20:44:57 -0500

    sliders and presale date

--- backend/src/config.js
+++ backend/src/config.js
@@ -135,7 +135,7 @@ const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-27T15:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-24T15:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xF74F296b28eDcA9BEa60738BdeD41cD4d8218986"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
